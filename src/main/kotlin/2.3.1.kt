65897ef79477dce5fcd358d88ead21b5cc806d94
Павел Сотников <pax4400@mail.ru>
2022-04-19 10:46:58 +0300

    correction

--- src/main/kotlin/2.3.1.kt
+++ src/main/kotlin/2.3.1.kt
@@ -1,18 +1,16 @@
 fun main() {
-    println("Hello Netology=)")
+
 }
-
-
 object WallService {
     private var posts = emptyArray<Post>()
 
     fun add(post: Post): Post {
-        if (!posts.isEmpty()) {
+        if (posts.isNotEmpty()) {
             val oldId = posts.last().id
             val postCopy = post.copy(id = oldId + 1);
             posts += postCopy
         } else {
-            posts += post.copy(1)
+            posts += post.copy(id = 1)
         }
         return posts.last()
     }
@@ -57,9 +55,9 @@ data class Post(
     val donut: Donut,
     val postponedId: Int,
 
-    val postSource: Object?,
+    val postSource: Any?,
     val geo: Geo,
-    val copyHistory: Array<Object>?,
+    val copyHistory: Array<Any>?,
 
     val attachments: Array<Attachment>?
 
@@ -111,7 +109,7 @@ data class Post(
     data class Geo(
         val type: String,
         val coordinates: String,
-        val place: Object?
+        val place: Any?
     )
 
     sealed interface Attachment
@@ -135,7 +133,7 @@ data class Post(
                 val width: Int
                 val withPadding: Int = 1
             },
-            val date: Integer
+            val date: Int
             // остальные поля не включил
         ) : Type()
 
@@ -155,7 +153,7 @@ data class Post(
             val ownerId: Int,
             val userId: Int,
             val text: String,
-            val date: Integer,
+            val date: Int,
             val sizes: () -> Unit = {
                 val typeC: String
                 val url: String
@@ -202,5 +200,81 @@ data class Post(
                 )
             }
         }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Post
+
+        if (id != other.id) return false
+        if (ownerId != other.ownerId) return false
+        if (fromId != other.fromId) return false
+        if (createdBy != other.createdBy) return false
+        if (date != other.date) return false
+        if (text != other.text) return false
+        if (replyOwnerId != other.replyOwnerId) return false
+        if (replyPostId != other.replyPostId) return false
+        if (friendsOnly != other.friendsOnly) return false
+        if (comments != other.comments) return false
+        if (copyright != other.copyright) return false
+        if (likes != other.likes) return false
+        if (reposts != other.reposts) return false
+        if (view != other.view) return false
+        if (postType != other.postType) return false
+        if (signerId != other.signerId) return false
+        if (canPin != other.canPin) return false
+        if (canDelete != other.canDelete) return false
+        if (canEdit != other.canEdit) return false
+        if (isPinned != other.isPinned) return false
+        if (markedAsAds != other.markedAsAds) return false
+        if (isFavorite != other.isFavorite) return false
+        if (donut != other.donut) return false
+        if (postponedId != other.postponedId) return false
+        if (postSource != other.postSource) return false
+        if (geo != other.geo) return false
+        if (copyHistory != null) {
+            if (other.copyHistory == null) return false
+            if (!copyHistory.contentEquals(other.copyHistory)) return false
+        } else if (other.copyHistory != null) return false
+        if (attachments != null) {
+            if (other.attachments == null) return false
+            if (!attachments.contentEquals(other.attachments)) return false
+        } else if (other.attachments != null) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id
+        result = 31 * result + ownerId
+        result = 31 * result + fromId
+        result = 31 * result + createdBy
+        result = 31 * result + date
+        result = 31 * result + text.hashCode()
+        result = 31 * result + replyOwnerId
+        result = 31 * result + replyPostId
+        result = 31 * result + friendsOnly.hashCode()
+        result = 31 * result + comments.hashCode()
+        result = 31 * result + copyright.hashCode()
+        result = 31 * result + likes.hashCode()
+        result = 31 * result + reposts.hashCode()
+        result = 31 * result + view.hashCode()
+        result = 31 * result + postType.hashCode()
+        result = 31 * result + signerId
+        result = 31 * result + canPin.hashCode()
+        result = 31 * result + canDelete.hashCode()
+        result = 31 * result + canEdit.hashCode()
+        result = 31 * result + isPinned.hashCode()
+        result = 31 * result + markedAsAds.hashCode()
+        result = 31 * result + isFavorite.hashCode()
+        result = 31 * result + donut.hashCode()
+        result = 31 * result + postponedId
+        result = 31 * result + (postSource?.hashCode() ?: 0)
+        result = 31 * result + geo.hashCode()
+        result = 31 * result + (copyHistory?.contentHashCode() ?: 0)
+        result = 31 * result + (attachments?.contentHashCode() ?: 0)
+        return result
     }
 }
